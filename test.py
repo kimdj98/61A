054bd2e2fadbf658da3a5ba04da2cb08ebd69d65
kdj <kimdj98g@gmail.com>
2021-05-04 00:02:47 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+#python programming test environment
+import numpy as np
+import matplotlib as plot
+
+a = np.arange(10)
+print(a)
+b = np.ones((2,3))
+print(b)
+
+print("hellloWorld")
+print("hellloWorld")
+print("hellloWorld")
+print("hellloWorld")
+print("hellloWorld")
+print("hellloWorld")
+
